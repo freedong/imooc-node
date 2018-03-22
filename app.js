7d6983757c7605fe27d6914f2e40ea4e2161228e
zxd <yrqc53719@163.com>
2018-03-22 11:01:21 +0800

    第一次提交

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 * @Author: Zhu Xiaodong
 * @Date:   2018-03-22 10:43:52
 * @Last Modified by:   Zhu Xiaodong
-* @Last Modified time: 2018-03-22 10:44:47
+* @Last Modified time: 2018-03-22 11:00:19
 */
 
 
+'use strict';
